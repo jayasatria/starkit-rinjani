e4a978b4deccb378c15275d12d4d9e6526102402
jayasatria <jayasatria9@gmail.com>
2020-11-22 10:16:18 +0800

    Tambah fitur input data pekerjaan

--- application/views/admin/add_job.php
+++ application/views/admin/add_job.php
@@ -2,17 +2,28 @@
     <div class="col-lg-10 mb-2">
         <form action="<?= base_url('admin/add_job'); ?>" method="POST">
             <div class="form-group">
-                <label for="exampleInputEmail1">Nama Pekerjaan</label>
-                <input type="text" class="form-control" id="nama_pekerjaan" name="nama_pekerjaan">
+                <label for="nama_pekerjaan">Nama Pekerjaan</label>
+                <input type="text" class="form-control" id="nama_pekerjaan" name="nama_pekerjaan" required>
                 <small id="emailHelp" class="form-text text-muted">Nama pekerjaan sesuai dengan kontrak</small>
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Nomor Kontrak</label>
-                <input type="text" class="form-control" id="no_kontrak" name="no_kontrak">
+                <label for="no_kontrak">Nomor Kontrak</label>
+                <input type="text" class="form-control" id="no_kontrak" name="no_kontrak" required>
             </div>
             <div class="form-group">
-                <label for="exampleInputEmail1">Nama Vendor</label>
-                <input type="text" class="form-control" id="vendor" name="vendor">
+                <label for="vendor">Nama Vendor</label>
+                <input type="text" class="form-control" id="vendor" name="vendor" required>
+            </div>
+            <div class="row">
+                <div class="form-group col-5">
+                    <label for="tgl_mulai">Tanggal Mulai</label>
+                    <input type="date" class="form-control" id="tgl_mulai" name="tgl_mulai" required>
+                </div>
+                <div class="form-group col-5">
+                    <label for="tgl_selesai">Tanggal Selesai</label>
+                    <input type="date" class="form-control" id="tgl_selesai" name="tgl_selesai" required>
+                </div>
+
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
