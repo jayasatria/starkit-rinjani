76d9b720b86262a72c454bdc3f8ec8c382d9ae98
jayasatria <jayasatria9@gmail.com>
2020-11-23 09:22:27 +0800

    Ganti nama controller Job

--- application/controllers/Job.php
+++ application/controllers/Job.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Worker extends CI_Controller
+class Job extends CI_Controller
 {
     public function __construct()
     {
--- application/views/worker/index.php
+++ application/views/worker/index.php
@@ -1,1 +1,3 @@
-<h1>Hello world</h1>
+<div class="container">
+    <h2>Anda Sedang Mengerjakan :</h2>
+</div>
