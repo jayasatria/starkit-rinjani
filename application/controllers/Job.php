f07c83fe3ea18613e324c54d45417401fe370d7d
jayasatria <jayasatria9@gmail.com>
2020-11-23 09:38:26 +0800

    membuat view job desk

--- application/controllers/Job.php
+++ application/controllers/Job.php
@@ -11,7 +11,10 @@ class Job extends CI_Controller
     public function index()
     {
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        $data['title'] = 'My Profile';
+        $data['title'] = 'Job Description';
+        $user = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->db->where('user_name', $user['email']);
+        $data['pekerjaan'] = $this->db->get('pekerjaan')->row_array();
         $this->load->view('template/header', $data);
         $this->load->view('template/sidebar', $data);
         $this->load->view('template/topbar', $data);
--- application/views/worker/index.php
+++ application/views/worker/index.php
@@ -1,3 +1,48 @@
 <div class="container">
-    <h2>Anda Sedang Mengerjakan :</h2>
+
+    <div class="row">
+
+        <h1>Detail Pekerjaan </h1>
+        <h6> <a href="<?= base_url('admin/editJob/') . $pekerjaan['id']; ?>" class="badge badge-warning mt-3 ml-3">edit</a></h6>
+    </div>
+    <a href="<?= base_url('admin/rendal'); ?>" class="badge badge-primary">Back <i class="fas fa-reply"></i></a>
+    <?= $this->session->flashdata('message'); ?>
+
+    <table class="table mt-5">
+
+        <tr>
+            <th scope="col" style="width: 150px;">Nama Pekerjaan</th>
+            <td><?= $pekerjaan['nama_pekerjaan']; ?></td>
+        </tr>
+        <tr>
+            <th scope="col">Nomor Kontrak</th>
+            <td><?= $pekerjaan['no_kontrak']; ?></td>
+        </tr>
+        <tr>
+            <th scope="col">Vendor</th>
+            <td><?= $pekerjaan['vendor']; ?></td>
+        </tr>
+        <tr>
+            <th scope="col">Username</th>
+            <td><?= $pekerjaan['user_name']; ?></td>
+        </tr>
+        <tr>
+            <th scope="col">Password</th>
+            <td><?= $pekerjaan['password']; ?></td>
+        </tr>
+        <tr>
+            <th scope="col">Tanggal Mulai</th>
+            <td><?= $pekerjaan['tgl_mulai']; ?></td>
+        </tr>
+        <tr>
+            <th scope="col">Tanggal Selesai</th>
+            <td><?= $pekerjaan['tgl_selesai']; ?></td>
+        </tr>
+        <tr>
+            <th scope="col">Deskripsi Pekerjaan</th>
+            <td><?= $pekerjaan['deskripsi']; ?></td>
+        </tr>
+
+    </table>
+
 </div>
