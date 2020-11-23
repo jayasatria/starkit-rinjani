6a95479300f9a988b6e3f1a7547eed8b3a7b1509
jayasatria <jayasatria9@gmail.com>
2020-11-23 11:55:24 +0800

    menambahkan fitur upload jobdesk

--- application/views/worker/desk.php
+++ application/views/worker/desk.php
@@ -1,3 +1,11 @@
 <div class="contsiner">
     <a href="<?= base_url('assets/job_desk/job_desk.xlsx'); ?>" class="btn btn-success mb-2 ml-2"><i class="fas fa-file-excel"></i> Download Template Job Description</a>
+    <div class="row ml-2">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="exampleFormControlFile1"> Upload Job Description</label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+        </form>
+    </div>
 </div>
